28f4fc80019b4d4424a8c23dcc0c1196174e2aed
marius-florea <67599658+marius-florea@users.noreply.github.com>
2023-03-23 17:51:32 +0200

    gpt finishing touches
    added parameters for large and small model
    Head class
    MultiHeadAttention class
    FeedForward layer
    Block class
    GptModel itself
    
    Gpt-dev - notebook
    Gpt-dev_trials - notebook

--- NanoGpt/gpt.py
+++ NanoGpt/gpt.py
@@ -3,14 +3,41 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences wil 
-block_size = 8 # what is the maximum context length for predictions
-max_iters = 5000
-eval_interval = 500 
-learning_rate = 1e-3
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters =200
-n_embd = 32
+smallModel = True
+if smallModel:
+    batch_size = 32# how many independent sequences wil 
+    block_size = 8 # what is the maximum context length for predictions
+    max_iters = 5000
+    eval_interval = 500 
+    learning_rate = 1e-3
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    eval_iters =200
+    n_embd = 32
+    n_head = 4 # => every head has 384/6= 64 -> every head is 64 dimensional
+    n_layer = 3
+    dropout = 0.00
+
+else:
+    #large model need a strong gpu to train it like A100
+    batch_size = 64# how many independent sequences wil 
+    block_size = 256 # what is the maximum context length for predictions
+    max_iters = 5000
+    eval_interval = 500 
+    learning_rate = 3e-4
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    eval_iters =200
+    n_embd = 384
+    n_head = 6 # => every head has 384/6= 64 -> every head is 64 dimensional
+    n_layer = 6
+    dropout = 0.2
+
+# What dropout does is it randomly, every forward backward pass, shuts of some subset of neurons
+# so randomly drops them to zero, and trains without them and what this does effectively 
+# because the mask of what's being dropped out has changed every single forward backward pass
+# it ends up kind of training an ensemble of sub networks 
+# and then at test time everything is fully enabled and all of those subnetworks is merged into a single ensemble
+# if you want to think about it that way
+     
 # ---------------
 
 torch.manual_seed(1337)
@@ -58,6 +85,9 @@ def estimate_loss():
     return out
 class Head(nn.Module):
     """ one head of self attention """
+    """ key - what do i contain 
+        query - what i am looking for
+    """
 
     def __init__(self, head_size):
         super().__init__()
@@ -65,33 +95,78 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        
+        self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x) # (B, T, C)
         q = self.query(x) # (B, T, C)
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2, -1) * C**-0.5 # (B,T,C) @ (B,C,T) -> (B, T, T)
+        # compute attention scores ("affinities" aka wei matrix) # k.shape[-1] is head_size
+        wei = q @ k.transpose(-2, -1) * k.shape[-1]**-0.5 # (B,T,C) @ (B,C,T) -> (B, T, T)
         # print(self.tril)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
         wei = F.softmax(wei, dim=-1) # (B,T,T)
+        wei = self.dropout(wei)
         #perform the weighted aggregation of the values
         v = self.value(x) # (B, T, C)
         out = wei @ v # (B,T,T) @ (B,T,C) -> (B,T,C)
         return out
 
+class MultiHeadAttention(nn.Module):
+    """Multiple heads of self-attention in parrallel"""
 
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
+    def forward(self, x):
+        result = torch.cat([h(x) for h in self.heads], dim=-1) # concatenating over the channel dimension
+        result = self.dropout(self.proj(result))
+        return result
 
+class FeedForward(nn.Module):
+    """a simple linear layer followd by a non-linearity"""
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation"""
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd) 
+                
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+        
 # bigram language model
-class BigramLanguageModel(nn.Module):
+class GPTLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
         #each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd) # the self attetnion head 
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) #final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -100,8 +175,10 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) #(B, T, C) - batch, time, channel
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B, T, C)
-        x = self.sa_head(x) # apply one head of self attention (B,T,C)
-        logits = self.lm_head(x) # (B,T, vocab_size)
+        x = self.blocks(x)
+        x = self.ln_f(x) # (B, T, C )  - the last layer normalization layer
+        logits = self.lm_head(x) # (B,T, vocab_size) - linear layer
+
         if targets is None:
             loss = None
         else:
@@ -130,8 +207,10 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel()  
+model = GPTLanguageModel()  
 m = model.to(device) # will run on a GPU if you have one 
+#print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 
 #create a Pythorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
@@ -139,7 +218,7 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 for iter in range(max_iters):
 
     #every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0:
+    if iter % eval_interval == 0 or iter == max_iters - 1:
         losses = estimate_loss()
         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
     
